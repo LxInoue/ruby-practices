0212ae293ea06f12cf124eb3a3e4f8d7838fda48
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-07 16:46:20 +0900

    説明コメントを削除

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -6,7 +6,6 @@ today = Date.today
 year = today.year
 month = today.month
 
-# オプションの読み込みと入力チェック
 opt = OptionParser.new
 opt.on("-y YEAR", Integer, "年を指定（例：2025）") { |y| year = y }
 opt.on("-m MONTH", Integer, "月を指定（例：3）") { |m| month = m }
@@ -24,26 +23,21 @@ unless (1970..2100).include?(year)
   abort("エラー：-yは1970〜2100の範囲で指定してください。")
 end
 
-# 年月日の作成とチェック
 begin
   first_date = Date.new(year, month, 1)
 rescue ArgumentError
   abort("エラー：不正な年または月が指定されました。")
 end
 
-# ヘッダーの出力
 puts "#{first_date.month}月 #{first_date.year}".center(20)
 puts "日 月 火 水 木 金 土"
 
-# 1日の曜日まで空白を出力
 print "   " * first_date.wday
 
-# 最終日を算出して出力
 last_day = Date.new(first_date.year, first_date.month, -1).day
 display_date = first_date
 (1..last_day).each do |day|
   if today == display_date
-    # 今日の日付の場合は反転表示させる。
     print "\e[7m" + day.to_s.rjust(2) + "\e[0m "
   else
     print day.to_s.rjust(2) + " "
