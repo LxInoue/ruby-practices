0042349500abc85bc87f8a5ee0e6a6682bbb7fcb
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-12 09:14:27 +0900

    times.sumを使うように修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -7,22 +7,24 @@ scores = score.split(',')
 shots = scores.map { |s| s == 'X' ? 10 : s.to_i }
 
 # 1～9フレームの処理 (10フレーム目は処理が異なるため後で作成)
-point = 0
 current_idx = 0
-9.times do
+point = 9.times.sum do
   if shots[current_idx] == 10
     # ストライク：次のフレームの2投分をボーナスとして加算。2投目は投げない。
-    point += shots[current_idx..(current_idx + 2)].sum
-    current_idx += 1
+    next_shots_count = 3
+    step = 1
   elsif shots[current_idx] + shots[current_idx + 1] == 10
     # スペア：次のフレームの1投分をボーナスとして加算
-    point += shots[current_idx..(current_idx + 2)].sum
-    current_idx += 2
+    next_shots_count = 3
+    step = 2
   else
     # 通常：2投分の合計
-    point += shots[current_idx..(current_idx + 1)].sum
-    current_idx += 2
+    next_shots_count = 2
+    step = 2
   end
+  frame_score = shots[current_idx, next_shots_count].sum
+  current_idx += step
+  frame_score
 end
 
 # 10フレーム目はそのまま追加 (ストライクかスペアだった場合は最大3投のためまとめて処理)
